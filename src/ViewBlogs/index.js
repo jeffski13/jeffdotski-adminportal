6244bfb484977ed7410169b93dab27441e257c82
jeffski13 <jeffskiosu@gmail.com>
2018-11-26 09:10:14 -0600

    added the trips selection dropdown component to the view blogs screen

--- src/ViewBlogs/index.js
+++ src/ViewBlogs/index.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import { FormGroup, ControlLabel, FormControl, ButtonToolbar, Button, DropdownButton, MenuItem } from 'react-bootstrap';
 import 'react-datepicker/dist/react-datepicker.css';
-
-import { getBlogs } from '../aws/blog';
-import { getTrips } from '../aws/trips';
-import BlogList from './BlogList';
-
-import { STATUS_LOADING, STATUS_FAILURE, STATUS_SUCCESS } from '../aws/networkConsts';
-import Indicator from '../aws/Indicator';
 import CircularProgress from 'material-ui/Progress/CircularProgress';
 
+import Indicator from '../aws/Indicator';
+import { STATUS_LOADING, STATUS_FAILURE, STATUS_SUCCESS } from '../aws/networkConsts';
+import { getBlogs } from '../aws/blog';
+import BlogList from './BlogList';
+import TripsDropdown from '../Trips/TripsDropdown'
 import './styles.css';
 
 class ViewBlogs extends Component {
@@ -20,8 +18,7 @@ class ViewBlogs extends Component {
 			trip: 'Chile-2017',
 			blogData: [],
 			status: null,
-			availableTrips: [],
-			tripIndexSelected: -1
+			tripId: null
 		};
 	}
 
@@ -29,43 +26,13 @@ class ViewBlogs extends Component {
 		this.setState({ trip: e.target.value });
 	}
 
-	componentDidMount() {
-		//get a list of all the trips when app starts
-		this.setState({ status: STATUS_LOADING });
-		getTrips((err, data) => {
-			if (err) {
-				console.log(err);
-				this.setState({ status: STATUS_FAILURE });
-				return;
-			}
-			this.setState({
-				availableTrips: data,
-				status: null
-			});
-		});
-	}
-
-	renderTripOptions = (nextTripItem, index) => {
-		return (
-			<MenuItem
-				key={`${index}-${nextTripItem.name}`}
-				eventKey={index}
-				onSelect={() => {
-					this.onTripSelected(index);
-				}}
-			>
-				{nextTripItem.name}
-			</MenuItem>
-		);
-	}
-
-	onTripSelected = (tripIndex) => {
+	onTripSelected = () => {
 		this.setState({
-			tripIndexSelected: tripIndex,
-			status: STATUS_LOADING
-		}, () => {
+			status: STATUS_LOADING,
+			blogData: []
+		}, ()=>{
 			//get list of blogs by trip name from server
-			getBlogs(this.state.availableTrips[this.state.tripIndexSelected].id, (err, data) => {
+			getBlogs(this.state.tripId, (err, data) => {
 				if (err) {
 					console.log(err);
 					this.setState({ status: STATUS_FAILURE });
@@ -82,30 +49,24 @@ class ViewBlogs extends Component {
 
 	render() {
 
-		let tripDropdownValue = "Selected Trip";
-		if (this.state.tripIndexSelected > -1 && this.state.availableTrips.length > 0) {
-			tripDropdownValue = this.state.availableTrips[this.state.tripIndexSelected].name;
-		}
-
 		return (
 			<div className="ViewBlogs">
-				<ButtonToolbar>
-					<DropdownButton
-						title={tripDropdownValue}
-						id="trips-dropdown"
-						disabled={this.state.status === STATUS_LOADING}
-					>
-						{this.state.availableTrips.map(this.renderTripOptions)}
-					</DropdownButton>
-					<div>
-						{(this.state.status === STATUS_LOADING)
-							&& <CircularProgress />}
-						{(this.state.status === STATUS_SUCCESS)
-							&& <Indicator success={true} />}
-						{(this.state.status === STATUS_FAILURE)
-							&& <Indicator success={false} />}
-					</div>
-				</ButtonToolbar>
+				<TripsDropdown
+					sortAlphabetically={false}
+					onTripReturned={(tripInfoReturned) => {
+						this.setState({
+							tripId: tripInfoReturned.id
+						});
+						this.onTripSelected();
+					}} />
+				<div id="getBlogsIndicator">
+					{(this.state.status === STATUS_LOADING)
+						&& <CircularProgress />}
+					{(this.state.status === STATUS_SUCCESS)
+						&& <Indicator success={true} />}
+					{(this.state.status === STATUS_FAILURE)
+						&& <Indicator success={false} />}
+				</div>
 				<BlogList blogsArr={this.state.blogData} />
 			</div>
 		);
